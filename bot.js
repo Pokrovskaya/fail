741102aed3950faacbef1b84c7e5324939c15392
Flairie <37420327+Flairie@users.noreply.github.com>
2018-03-16 00:28:40 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -21,10 +21,12 @@ client.on('message', msg => {
 
   if (msg.content === 'What is the battle system?') {
     msg.reply('Battle System - machanics of Otradnoe that will be added much later');
-
+  }
+  
   if (msg.content === 'What is the otradnoye-bot?') {
     msg.reply('Damned Otradnoye-bot is the fokkin piece of shut thats fokkin never worked right! Dumnit!');
   }
+  
   if (msg.content === '/mymoney' && msg.channel.topic === 'Bank-de-koval') {
     msg.reply('Shut up!');
   }
