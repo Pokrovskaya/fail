8542e9549e758e019594f89423d9449ea37c0fd2
Flairie <37420327+Flairie@users.noreply.github.com>
2018-03-15 23:37:03 +0300

    Huh?
    Why do I need to write this?

---
+++ bot.js
@@ -0,0 +1,32 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+var disgust = 1;
+
+client.on('ready', () => {
+  console.log(`Logged in as ${423868710940311552}!`);
+});
+
+client.on('message', msg => {
+  if (msg.content === 'ping') {
+    msg.reply('Pong!');
+  }
+
+  if (msg.content === 'What is the wahtang?') {
+    msg.reply('I, Wahtang, am the keeper of knowleges of Otradnoye. You can ask me about anything here.');
+  }
+
+  if (msg.content === 'What is the legendary roles?') {
+    msg.reply('Legendary role is the role what can be used by an only one person in Otradnoye');
+  }
+
+  if (msg.content === 'What is the battle system?') {
+    msg.reply('Battle System - machanics of Otradnoe that will be added much later');
+  }
+
+  if (msg.content === 'What is the otradnoye-bot?') {
+    msg.reply('Damned Otradnoye-bot is the fokkin piece of shut thats fokkin never worked right! Dumnit!');
+  }
+});
+
+client.login('NDIzODY4NzEwOTQwMzExNTUy.DYwmAw.Cp_TfxpssJjtDBG2XKuYnYEcwC0');
+
