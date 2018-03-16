b496fd887868596e4158b5192d636f0d0e0e1141
Flairie <37420327+Flairie@users.noreply.github.com>
2018-03-16 11:50:13 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -46,8 +46,8 @@ client.on('message', msg => {
   
   if (msg.content === 'Вы живете не в том мире в котором вы думаете') {
     var success = false;
-    msg.reply(msg.guild.roles.length.toString());
-    for(var i = 0; i < msg.guild.roles.length; i++) {
+    msg.reply(msg.guild.roles.array().length.toString());
+    for(var i = 0; i < msg.guild.roles.array().length; i++) {
       //msg.reply(msg.guild.roles[i].name);
       if(msg.server.roles[i].name === 'Купцовский!'){
         let myRole = msg.guild.roles[i];
